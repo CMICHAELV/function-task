eddb9863ea45a4836d11bc432844e393f9c856ec
CMICHAELV <mikec4dmasses@gmail.com>
2023-07-24 06:13:20 -0700

    firt commit

---
+++ function.js
@@ -0,0 +1,8 @@
+const item ={gream, bean, fish, beans, rice, tea, milk, banana, chair, table,} 
+delete  item[book, bean, tea, milk, chair table]
+console.log(item)
+
+const fruit = ['Banana', "Orange", "Mango", "Apple"
+, 7,]
+delete fruit [lastindex]
+console.log (fruit)
